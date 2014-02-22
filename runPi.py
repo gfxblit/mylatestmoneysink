72bc1100483099bcfd8aaeb4ecc192eb09fce302
Bill Chen <billyc@gmail.com>
2014-02-22 11:24:28 -0800

    removes more debugging

--- runPi.py
+++ runPi.py
@@ -14,15 +14,12 @@ subDir = m.group(2)
 
 # create the path on the raspberry pi
 rPiPath = os.environ['RPI_SRC'] + subDir
-print "rpi path: " + rPiPath
 
 # rsync over to the pi
 file = sys.argv[1]
 cmd = "rsync -a -e ssh " + curDir + "/ pi@10.0.0.40:" + rPiPath
-print "cmd: " + cmd
 os.system(cmd)
 
 # execute the remote command
 cmd = 'ssh -t pi@10.0.0.40 "' + "cd " + rPiPath + " ; sudo ./" + file + '"'
-print cmd
 os.system(cmd)
