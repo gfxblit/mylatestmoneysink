99fccc4e0bbe855127eaf700de04a64102daa0ee
Bill Chen <billyc@gmail.com>
2014-02-22 14:05:10 -0800

    adjust some pins according to current wiring and remap keys to awsd

--- MotorDriver.py
+++ MotorDriver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Please see for more info:
 # http://mylatestmoneysink.blogspot.com/2014/01/driving-1100-motors-with-tb6612fng.html
 
@@ -6,28 +6,31 @@ import RPi.GPIO as GPIO
 import sys
 import time
 
-class MotorDriver:
+class MotorDriver:        
     # these use BCM pin layout
-    _pins = [[2, 3], [4, 17]]
-
+    _pins = [[27, 22], [4, 17]]
+    
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self._pins[0][0], GPIO.OUT)
         GPIO.setup(self._pins[0][1], GPIO.OUT)
         GPIO.setup(self._pins[1][0], GPIO.OUT)
         GPIO.setup(self._pins[1][1], GPIO.OUT)
+	    # start with everything stopped, in case something was driving the pins before
+        self.stop()
 
     def __del__(self):
+        self.stop()
         GPIO.cleanup()
 
     def _outputToMotor(self, motor, bits):
         GPIO.output(self._pins[motor][0], bits[0])
         GPIO.output(self._pins[motor][1], bits[1])
-    
+ 
     # period is in seconds, if < 0, will run until a new command is issued
     def forward(self, period):
         self._outputToMotor(0, [GPIO.LOW, GPIO.HIGH])
-        self._outputToMotor(1, [GPIO.LOW, GPIO.HIGH])
+        self._outputToMotor(1, [GPIO.HIGH, GPIO.LOW])
         if period > 0:
             time.sleep(period)
             self.stop()
@@ -38,39 +41,43 @@ class MotorDriver:
 
     def reverse(self, period):
         self._outputToMotor(0, [GPIO.HIGH, GPIO.LOW])
-        self._outputToMotor(1, [GPIO.HIGH, GPIO.LOW])
+        self._outputToMotor(1, [GPIO.LOW, GPIO.HIGH])
+
         if period > 0:
             time.sleep(period)
             self.stop()
     
-    def rotateCW(self, period):
-        self._outputToMotor(0, [GPIO.HIGH, GPIO.LOW])
-        self._outputToMotor(1, [GPIO.LOW,  GPIO.HIGH])        
-        if period > 0:
-            time.sleep(period)
-            self.stop()
-    
-    def rotateCCW(self, period):
+    def turnLeft(self, period):
         self._outputToMotor(0, [GPIO.LOW, GPIO.HIGH])
-        self._outputToMotor(1, [GPIO.HIGH,GPIO.LOW])        
+        self._outputToMotor(1, [GPIO.LOW,  GPIO.HIGH])
+
         if period > 0:
             time.sleep(period)
             self.stop()
-                
+    
+    def turnRight(self, period):
+        self._outputToMotor(0, [GPIO.HIGH, GPIO.LOW])
+        self._outputToMotor(1, [GPIO.HIGH,GPIO.LOW])    
+
+        if period > 0:
+            time.sleep(period)
+            self.stop()
+
 if __name__ == "__main__":
     motorDriver = MotorDriver()
+    
     while 1:
-       cmd = raw_input('cmd (f=forward r=reverse o=off c=cw w=ccw q=quit): ')
-       if cmd == 'f':
-           motorDriver.forward(0.5)
-       if cmd == 'r':
-           motorDriver.reverse(0.5)
+       cmd = input('cmd (w=forward s=reverse a=turn left d=turn right o=off q=quit): ')
+       if cmd == 'w':
+           motorDriver.forward(0)
+       if cmd == 's':
+           motorDriver.reverse(0)
        if cmd == 'o':
            motorDriver.stop()
-       if cmd == 'c':
-           motorDriver.rotateCW(0.5)
-       if cmd == 'w':
-           motorDriver.rotateCCW(0.5)
+       if cmd == 'd':
+           motorDriver.turnRight(0.2)
+       if cmd == 'a':
+           motorDriver.turnLeft(0.2)
        if cmd == 'q':
            sys.exit()
 
