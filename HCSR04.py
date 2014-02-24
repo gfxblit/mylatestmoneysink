4f0e55abb59fefffda6389e4e6312dff86b6d951
Bill Chen <billyc@gmail.com>
2014-02-23 17:01:25 -0800

    fixed tabbing problems and a bug where start didn't get set

--- HCSR04.py
+++ HCSR04.py
@@ -11,30 +11,31 @@ class HCSR04:
         self._echoPin = echoPin
         GPIO.setup(self._triggerPin, GPIO.OUT)
         GPIO.setup(self._echoPin, GPIO.IN)
-	GPIO.output(self._triggerPin, GPIO.LOW)
+        GPIO.output(self._triggerPin, GPIO.LOW)
 
     def getRangeInCentimeters(self):
         # issue a 10uS pulse
         GPIO.output(self._triggerPin, GPIO.HIGH)
-	time.sleep(0.00001)
+        time.sleep(0.00001)
         GPIO.output(self._triggerPin, GPIO.LOW)
         
         # wait for the echo
+        start = time.time()
         while GPIO.input(self._echoPin) == 0:
             start = time.time()
 
         while GPIO.input(self._echoPin) == 1:
             stop = time.time()
         
-        return (stop - start)  * 17241.37931034482759 # 1000000 / 58
+        return (stop - start)  * 1000000.0 / 58.0;
          
     def __del__(self):
         GPIO.cleanup()
     
 	
 if __name__ == "__main__":
-    hcsr04 = HCSR04(17, 4)
+    hcsr04 = HCSR04(18, 23)
     raw_input("waiting to start")
     while 1:
-        print "range: " + str(hcsr04.getRangeInCentimeters())
+        print("range: " + str(hcsr04.getRangeInCentimeters()))
         time.sleep(1)
