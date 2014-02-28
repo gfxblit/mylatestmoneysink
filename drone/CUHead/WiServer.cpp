bd705f6e8408546e4b455141460099eeebc0b3be
Bill Chen <billyc@gmail.com>
2014-02-27 21:42:35 -0800

    Initialization now exits after 10 minutes. This is to prevent the driver from sitting in an infinite loop.

--- drone/CUHead/WiServer.cpp
+++ drone/CUHead/WiServer.cpp
@@ -103,8 +103,13 @@ void Server::init(pageServingFunction function) {
 	PCMSK0 |= (1<<PCINT0);
 #endif
 
+    unsigned long start = millis();
 	while(zg_get_conn_state() != 1) {
 		zg_drv_process();
+        unsigned long current = millis();
+        if (current - start > 600000) { // 10 minutes
+            return;
+        }
 	}
 
 	// Start the stack
